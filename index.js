ac617ffba65a2910a2efb338f902760e6c2d7978
rojer <rojerchen@qq.com>
2024-03-29 17:36:05 +0800

    移除对服务端的依赖

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ let tray = null;
 let defaultPrinter;
 let printers = printer.getPrinters();
 let status = "等待打印任务...";
+
 app.whenReady().then(() => {
   const trayIcon = path.join(__dirname, "tray.png");
   tray = new Tray(trayIcon);
@@ -28,7 +29,7 @@ app.whenReady().then(() => {
 
 const refreshMenu = () => {
   const openAtLogin = app.getLoginItemSettings().openAtLogin;
-  console.log("load openAtLogin", openAtLogin);
+  // console.log("load openAtLogin", openAtLogin);
 
   const contextMenu = Menu.buildFromTemplate([
     {
@@ -68,12 +69,6 @@ const refreshMenu = () => {
     },
 
     {
-      label: "测试打印",
-      click: () => {
-        testPrint();
-      },
-    },
-    {
       label: "开启自动启动",
       type: "checkbox",
       checked: openAtLogin,
@@ -99,98 +94,24 @@ const refreshMenu = () => {
   tray.setContextMenu(contextMenu);
 };
 
-const testPrint = () => {
-  print(
-    [
-      {
-        type: "text",
-        data: "${text}",
-        props: {
-          size: 2,
-        },
-      },
-      {
-        type: "line",
-        data: "*",
-      },
-      {
-        type: "img",
-        data: "https://www.w3school.com.cn/i/eg_tulip.jpg",
-        props: {
-          width: 150,
-        },
-      },
-      {
-        type: "qrcode",
-        data: "测试二维码，如果您看到此段话代表二维码有效！",
-        props: {
-          width: 150,
-        },
-      },
-      {
-        type: "table",
-        data: [
-          {
-            label: "测试标题",
-            value: "测试内容",
-          },
-        ],
-        props: {
-          hideHeader: true,
-          header: [
-            {
-              dataIndex: "label",
-              cols: 8,
-              align: "left",
-            },
-            {
-              dataIndex: "value",
-              cols: 24,
-              align: "right",
-            },
-          ],
-        },
-      },
-      {
-        type: "line",
-        data: "*",
-      },
-      {
-        type: "text",
-        data: "测试结束",
-      },
-      {
-        type: "line",
-        data: "=",
-      },
-    ],
-    {
-      text: "打印测试",
-    },
-    {
-      flag: "test",
-      width: 32,
-      style: {
-        align: "center",
-      },
-    }
-  );
-};
+const testPrint = () => {};
+
 const initws = () => {
   const wss = new WebSocketServer({ port: 14529 });
   wss.on("connection", (client) => {
-    console.log("client...connection", client.id);
+    // console.log("client...connection", client.id);
     client.on("message", (message) => {
       try {
         const data = JSON.parse(message);
-        print(data.template, data.data, data.options);
+        // console.log("client...message", data);
+        print(data);
       } catch (error) {
-        //...
+        // console.log("client...message error", error);
       }
     });
 
     client.on("close", () => {
-      console.log("client...close", client.id);
+      // console.log("client...close", client.id);
     });
   });
   wss.on("error", (err) => {
@@ -202,44 +123,29 @@ const initws = () => {
   });
 };
 
-const print = async (template, data, options) => {
-  const apiurl = store.get("api") || "https://print.freesailing.cn/parse";
-  if (!apiurl)
-    return dialog.showMessageBox({
-      message: "没有配置打印接口服务器",
-    });
+const print = async (resdata) => {
   try {
-    const response = await axios({
-      url: apiurl,
-      method: "POST",
-      data: {
-        template,
-        data,
-        options,
+    let arraybuffer = base64.decode(resdata.data);
+    if (resdata.type === "gzip") {
+      arraybuffer = pako.ungzip(arraybuffer);
+    }
+    printer.printDirect({
+      printer: defaultPrinter,
+      data: arraybuffer,
+      type: "RAW",
+      success: function (jobID) {
+        status = `打印任务[${jobID}]中`;
+        refreshMenu();
+      },
+      error: function (err) {
+        status = `打印失败...`;
+        refreshMenu();
       },
     });
-
-    const { data: resdata } = response;
-    if (resdata.data && resdata.type) {
-      let arraybuffer = base64.decode(resdata.data);
-      if (resdata.type === "gzip") {
-        arraybuffer = pako.ungzip(arraybuffer);
-      }
-      printer.printDirect({
-        printer: defaultPrinter,
-        data: arraybuffer,
-        type: "RAW",
-        success: function (jobID) {
-          status = `打印任务[${jobID}]中`;
-          refreshMenu();
-        },
-        error: function (err) {
-          status = `打印失败...`;
-          refreshMenu();
-        },
-      });
-    }
   } catch (error) {
     console.log(error.response);
+    return dialog.showMessageBox({
+      message: error.response,
+    });
   }
 };
